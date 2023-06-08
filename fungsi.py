7ba8365fb9444ada5167582b7c1c3c3fd8202584
daffaulhaqqi <mdaffa32@gmail.com>
2023-06-08 13:15:16 +0700

    menambahkan fungsi

---
+++ fungsi.py
@@ -0,0 +1,79 @@
+import json
+import datetime
+
+def input_data(nama, berat_laundry, jenis_laundry, jenis_pewangi, jarak):
+    with open("data.json", "r") as f:
+        x = json.load(f)
+        pewangi = x["pewangi"]
+    tanggal = datetime.datetime.now()
+    if jarak <= 3:
+        harga_delivery = 0
+    elif jarak > 3:
+        harga_delivery = (jarak-3)*3000
+
+    if jenis_laundry == "reguler":
+        waktu_selesai = datetime.datetime.now() + datetime.timedelta(days=3)
+        harga_laundry = 3500
+    else:
+        waktu_selesai = datetime.datetime.now() + datetime.timedelta(hours=12)
+        harga_laundry = 5000
+    total_biaya = (berat_laundry * harga_laundry) + pewangi[jenis_pewangi] + harga_delivery
+    data_pelanggan = {
+            "nama": nama,
+            "berat_laundry": berat_laundry,
+            "jenis_laundry": jenis_laundry,
+            "jenis_pewangi": jenis_pewangi,
+            "harga_laundry_per_kg": harga_laundry,
+            "harga_pewangi" : pewangi[jenis_pewangi],
+            "tanggal_laundry" : tanggal.strftime("%Y-%m-%d %H:%M:%S"),
+            "jarak" : jarak,
+            "harga delivery": harga_delivery,
+            "total_biaya": total_biaya,
+            "estimasi_waktu_selesai": waktu_selesai.strftime("%Y-%m-%d %H:%M:%S")
+            }
+    print(json.dumps(data_pelanggan, indent=4))
+    with open("data.json", 'r') as f:
+        jsondata = f.read()
+        data = json.loads(jsondata)
+        data["pelanggan"].append(data_pelanggan)
+        a = json.dumps(data["pelanggan"], indent=4)
+    
+    with open("data.json", "w") as f:
+        f.write(json.dumps(data, indent=4))
+        
+def tampilkan_data(nama):
+    with open("data.json", "r") as f:
+        data = json.load(f)
+        found = False
+        for pelanggan in data["pelanggan"]:
+            if pelanggan["nama"] == nama:
+                print('Nama Pelanggan:', pelanggan["nama"])
+                print('Tanggal Laundry:', pelanggan["tanggal_laundry"])
+                print('Berat Laundry:', pelanggan["berat_laundry"])
+                print('Jenis Laundry:', pelanggan["jenis_laundry"])
+                print('Jenis Pewangi:', pelanggan["jenis_pewangi"])
+                print('Harga Laundry Per Kg:', pelanggan["harga_laundry_per_kg"])
+                print('Harga Pewangi:', pelanggan["harga_pewangi"])
+                print('Total Biaya:', pelanggan["total_biaya"])
+                print('Estimasi Waktu Selesai: ', pelanggan["estimasi_waktu_selesai"])
+                found = True
+                break
+        else:
+            if not found:
+                print("Tidak Ada Nama")
+
+def hapus_data(nama):
+    with open("data.json", "r") as f:
+        data = json.load(f)
+        found = False
+        for pelanggan in data["pelanggan"]:
+            if pelanggan["nama"] == nama:
+                data["pelanggan"].remove(pelanggan)
+                a = json.dumps(data["pelanggan"], indent=4)
+                with open("data.json", "w") as f:
+                    f.write(json.dumps(data, indent=4))
+                print('Data Dihapus')
+                found = True
+        else:
+                if not found:
+                    print("Data tidak ada!")
